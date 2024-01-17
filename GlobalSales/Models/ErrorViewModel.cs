fa5bc0a41b3b988ce02d648af3f433b714f53e68
ezreth <diego.loport@gmail.com>
2024-01-16 23:26:15 -0500

    Creación del proyecto

---
+++ GlobalSales/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GlobalSales.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
